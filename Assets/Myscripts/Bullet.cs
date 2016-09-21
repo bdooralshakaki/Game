0e7afe670e62b180ff3abdb425ff652f47b67b96
bdooralshakaki <bdoor.alshakaki@gmail.com>
2016-09-21 17:48:00 +0100

    first commit

---
+++ Assets/Myscripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+
+    public float bulletSpeed = 5f;
+    void Update()
+    {
+        transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime);
+    }
+
+    void OnBecameInvisible()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
